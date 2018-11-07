536d2c9278a9297898185ac01d7dc788002e8830
ODYSOLVD\Vadivelan.Udayakumar <vadivelan_udayakumar@odysseussolutions.com>
2018-11-07 20:30:51 +0530

    Basic Empty Web Api Project

---
+++ myApp/myWebApiApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myWebApiApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
